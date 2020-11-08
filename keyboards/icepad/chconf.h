66faf22113b34c5fbdcb91170969a149d789ec23
icequbed <ben@adamsons.info>
2020-11-08 23:04:02 +0000

    added overrides for chconf and halconf

---
+++ keyboards/icepad/chconf.h
@@ -0,0 +1,2 @@
+#define CH_CFG_USE_TM                       FALSE
+#include_next "chconf.h"
---
+++ keyboards/icepad/halconf.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define HAL_USE_I2C FALSE
+#define HAL_USE_PWM FALSE
+#define HAL_USE_SPI FALSE
+#define HAL_USE_GPT FALSE
+#define HAL_USE_PWM FALSE
+#define HAL_USE_DAC FALSE
+
+#include_next "halconf.h"
